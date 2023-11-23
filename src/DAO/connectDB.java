82cdcf60f1ae8e109a05e1718c3b95fefdd1aaec
truongchinh1211 <nntchinh2001@gmail.com>
2023-11-23 23:00:53 +0700

    init

--- src/DAO/connectDB.java
+++ src/DAO/connectDB.java
@@ -27,7 +27,7 @@ public class connectDB {
     
     public Connection getConnect() {
         try {    
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food_store", "thanh", "123456");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/fast_food_store", "root", "");
             return conn;
         } catch (SQLException ex) {
             Logger.getLogger(connectDB.class.getName()).log(Level.SEVERE, null, ex);
